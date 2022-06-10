3beee767858de0c36b365d7c55b096f697a9ce93
astbel <astbel4633214@gmail.com>
2022-06-10 17:39:32 +0800

    UART跟通已確認可以在C#收到目前需要修正FLASH部分目前在寫入資料會錯誤

---
+++ UART_Hello/Debug/Core/Src/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/Flash.c \
+../Core/Src/main_uart.c \
+../Core/Src/stm32g4xx_hal_msp.c \
+../Core/Src/stm32g4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32g4xx.c \
+../Core/Src/uartMsg.c 
+
+OBJS += \
+./Core/Src/Flash.o \
+./Core/Src/main_uart.o \
+./Core/Src/stm32g4xx_hal_msp.o \
+./Core/Src/stm32g4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32g4xx.o \
+./Core/Src/uartMsg.o 
+
+C_DEPS += \
+./Core/Src/Flash.d \
+./Core/Src/main_uart.d \
+./Core/Src/stm32g4xx_hal_msp.d \
+./Core/Src/stm32g4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32g4xx.d \
+./Core/Src/uartMsg.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32G474xx -DDEBUG -c -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/Flash.d ./Core/Src/Flash.o ./Core/Src/Flash.su ./Core/Src/main_uart.d ./Core/Src/main_uart.o ./Core/Src/main_uart.su ./Core/Src/stm32g4xx_hal_msp.d ./Core/Src/stm32g4xx_hal_msp.o ./Core/Src/stm32g4xx_hal_msp.su ./Core/Src/stm32g4xx_it.d ./Core/Src/stm32g4xx_it.o ./Core/Src/stm32g4xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32g4xx.d ./Core/Src/system_stm32g4xx.o ./Core/Src/system_stm32g4xx.su ./Core/Src/uartMsg.d ./Core/Src/uartMsg.o ./Core/Src/uartMsg.su
+
+.PHONY: clean-Core-2f-Src
+
