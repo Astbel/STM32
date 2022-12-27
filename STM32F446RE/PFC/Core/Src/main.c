b8437759602eee69e5f2b07abec0042ca229011b
Astbel <astbel4633214@gmail.com>
2022-12-27 09:15:33 +0800

    PWM設定調整TIM_OCPOLARITY_LOW比較後low輸出否則會與設定輸出反向

--- STM32F446RE/PFC/Core/Src/main.c
+++ STM32F446RE/PFC/Core/Src/main.c
@@ -125,21 +125,21 @@ int main(void)
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  xTaskCreate((TaskFunction_t)start_task,
-              (char *)"start_task",
-              (uint16_t)START_TSK_SIZE,
-              (void *)NULL,
-              (UBaseType_t)START_TASK_PRO,
-              (TaskHandle_t *)&START_TASK_Handle);
+  // xTaskCreate((TaskFunction_t)start_task,
+  //             (char *)"start_task",
+  //             (uint16_t)START_TSK_SIZE,
+  //             (void *)NULL,
+  //             (UBaseType_t)START_TASK_PRO,
+  //             (TaskHandle_t *)&START_TASK_Handle);
 
-   HAL_TIM_Base_Start_IT(&htim10);
+  //  HAL_TIM_Base_Start_IT(&htim10);
   /* Start scheduler */
   /*TEST PWM PIN*/
-  // HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1); //PWM Master ClK
-  // HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_2);
-  // HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3); // Phase A
-  // HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_4);
-  // HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3); // Phase B
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1); //PWM Master ClK
+  HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3); // Phase A
+  HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_4);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3); // Phase B
   /*RTOS START*/
   // osKernelStart();
 
@@ -344,8 +344,8 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = PULSE1_VALUE;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.Pulse = 100;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_LOW;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
@@ -428,7 +428,7 @@ static void MX_TIM2_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = PULSE1_VALUE;
+  sConfigOC.Pulse = 100;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
